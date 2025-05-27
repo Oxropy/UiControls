6fb316260478da82b8564cbbb5b1f8dd865b410d
Alex <alex.pollich@t-online.de>
2025-05-27 23:17:45 +0200

    extend example for CellChangedEventArgs and GetDefaultCell

--- UiControlsExamples/MainWindow.xaml.cs
+++ UiControlsExamples/MainWindow.xaml.cs
@@ -1,23 +1,41 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UiControls;
 
 namespace UiControlsExamples;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        DynamicGrid.GetDefaultCell = () => new Border
+        {
+            Background = new SolidColorBrush(Colors.Gray)
+        };
+    }
+
+    private void DynamicGrid_OnCellChanged(object? sender, CellChangedEventArgs e)
+    {
+        switch (e.ChangeType)
+        {
+            case CellsChangeType.Add:
+                Console.WriteLine($"Cell at {e.Position.X}/{e.Position.Y} with DataContext {DataContext} was added");
+                break;
+            case CellsChangeType.Remove:
+                Console.WriteLine($"Cell at {e.Position.X}/{e.Position.Y} with DataContext {DataContext} was removed");
+                break;
+            case CellsChangeType.SizeIncreased:
+                Console.WriteLine($"Cell size at {e.Position.X}/{e.Position.Y} with DataContext {DataContext} was increased");
+                break;
+            case CellsChangeType.SizeDecreased:
+                Console.WriteLine($"Cell size at {e.Position.X}/{e.Position.Y} with DataContext {DataContext} was decreased");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
